NucleusPowered/Nucleus

Add way to get if someone is currently toggled into staff chat

nucleus-api/src/main/java/io/github/nucleuspowered/nucleus/api/module/staffchat/NucleusStaffChatService.java
@@ -5,15 +5,46 @@
 package io.github.nucleuspowered.nucleus.api.module.staffchat;
 
 import org.spongepowered.api.command.CommandSource;
+import org.spongepowered.api.command.source.ConsoleSource;
+import org.spongepowered.api.entity.living.player.User;
+import org.spongepowered.api.event.message.MessageChannelEvent;
 import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.channel.MessageReceiver;
 
 import java.util.Collection;
+import java.util.UUID;
 
 /**
  * Provides a way to get the Staff Chat message channel instance.
  */
 public interface NucleusStaffChatService {
+
+    /**
+     * Gets if the provided {@link MessageChannelEvent.Chat} is going to be
+     * sent to the staff chat channel.
+     *
+     * @param event The {@link MessageChannelEvent.Chat}
+     * @return true if so
+     */
+    boolean isDirectedToStaffChat(MessageChannelEvent.Chat event);
+
+    /**
+     * Gets if the given {@link CommandSource} is currently talking
+     * in staff chat.
+     *
+     * @param source The {@link CommandSource}
+     * @return true if so
+     */
+    boolean isCurrentlyChattingInStaffChat(CommandSource source);
+
+    /**
+     * Gets if a player with the given {@link UUID} is currently talking
+     * in staff chat.
+     *
+     * @param uuid The {@link UUID} of the {@link User}
+     * @return true if so
+     */
+    boolean isCurrentlyChattingInStaffChat(UUID uuid);
 
     /**
      * Gets the memebers of the Staff Chat channel.

nucleus-core/src/main/java/io/github/nucleuspowered/nucleus/services/interfaces/IChatMessageFormatterService.java
@@ -21,9 +21,13 @@ import java.util.UUID;
 @ImplementedBy(ChatMessageFormatterService.class)
 public interface IChatMessageFormatterService {
 
+    Optional<Channel> getNucleusChannel(CommandSource source);
+
     Optional<Channel> getNucleusChannel(UUID uuid);
 
     void setPlayerNucleusChannel(UUID uuid, @Nullable Channel channel);
+
+    NoExceptionAutoClosable setCommandSourceNucleusChannelTemporarily(CommandSource source, Channel channel);
 
     NoExceptionAutoClosable setPlayerNucleusChannelTemporarily(UUID uuid, Channel channel);
 

nucleus-core/src/main/java/io/github/nucleuspowered/nucleus/modules/staffchat/commands/StaffChatCommand.java
@@ -76,9 +76,14 @@ public class StaffChatCommand implements ICommandExecutor<CommandSource> {
                             .userPreferenceService()
                             .setPreferenceFor(pl, NucleusKeysProvider.VIEW_STAFF_CHAT, true);
                 } else {
-                    StaffChatMessageChannel.getInstance()
-                            .sendMessageFrom(context.getCommandSource(),
-                                    context.getServiceCollection().textStyleService().addUrls(toSend.get()));
+                    frame.pushCause(context.getCommandSourceUnchecked());
+                    // mostly to allow plugins to know that's what we're doing.
+                    try (NoExceptionAutoClosable c = this.chatMessageFormatterService
+                            .setCommandSourceNucleusChannelTemporarily(context.getCommandSource(), StaffChatMessageChannel.getInstance())) {
+                        StaffChatMessageChannel.getInstance()
+                                .sendMessageFrom(context.getCommandSource(),
+                                        context.getServiceCollection().textStyleService().addUrls(toSend.get()));
+                    }
                 }
 
                 return context.successResult();

OpalSoPL/Oleus

nucleus-api/src/main/java/io/github/nucleuspowered/nucleus/api/module/staffchat/NucleusStaffChatService.java
@@ -4,10 +4,20 @@
  */
 package io.github.nucleuspowered.nucleus.api.module.staffchat;
 
+<<<<<<< HEAD
 import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.text.Component;
+=======
+import org.spongepowered.api.command.CommandSource;
+import org.spongepowered.api.command.source.ConsoleSource;
+import org.spongepowered.api.entity.living.player.User;
+import org.spongepowered.api.event.message.MessageChannelEvent;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.channel.MessageReceiver;
+>>>>>>> 232bd2c4... Add way to get if someone is currently toggled into staff chat
 
 import java.util.Collection;
+import java.util.UUID;
 
 /**
  * Provides a way to get the Staff Chat message channel instance.
@@ -15,7 +25,34 @@ import java.util.Collection;
 public interface NucleusStaffChatService {
 
     /**
-     * Gets the memebers of the Staff Chat channel.
+     * Gets if the provided {@link MessageChannelEvent} is going to be
+     * sent to the staff chat channel.
+     *
+     * @param event The {@link MessageChannelEvent}
+     * @return true if so
+     */
+    boolean isDirectedToStaffChat(MessageChannelEvent event);
+
+    /**
+     * Gets if the given {@link Audience} is currently talking
+     * in staff chat.
+     *
+     * @param source The {@link Audience}
+     * @return true if so
+     */
+    boolean isCurrentlyChattingInStaffChat(Audience source);
+
+    /**
+     * Gets if a player with the given {@link UUID} is currently talking
+     * in staff chat.
+     *
+     * @param uuid The {@link UUID} of the {@link User}
+     * @return true if so
+     */
+    boolean isCurrentlyChattingInStaffChat(UUID uuid);
+
+    /**
+     * Gets the members of the Staff Chat channel.
      *
      * @return The {@link Audience}s who are members of the channel.
      */

nucleus-core/src/main/java/io/github/nucleuspowered/nucleus/services/interfaces/IChatMessageFormatterService.java
@@ -25,9 +25,13 @@ import java.util.UUID;
 @ImplementedBy(ChatMessageFormatterService.class)
 public interface IChatMessageFormatterService {
 
+    Optional<Channel> getNucleusChannel(Audience source);
+
     Optional<Channel> getNucleusChannel(UUID uuid);
 
     void setPlayerNucleusChannel(UUID uuid, @Nullable Channel channel);
+
+    NoExceptionAutoClosable setAudienceNucleusChannelTemporarily(Audience source, Channel channel);
 
     NoExceptionAutoClosable setPlayerNucleusChannelTemporarily(UUID uuid, Channel channel);
 
@@ -36,10 +40,6 @@ public interface IChatMessageFormatterService {
         default boolean willFormat() {
             return false;
         }
-
-        default void formatMessageEvent(final Player source, final MessageEvent.MessageFormatter formatters) {}
-
-        default void formatMessageEvent(final SystemSubject source, final MessageEvent.MessageFormatter formatters) {}
 
         default Audience receivers() {
             return Sponge.getServer();

nucleus-modules/src/main/java/io/github/nucleuspowered/nucleus/modules/staffchat/commands/StaffChatCommand.java
@@ -73,9 +73,14 @@ public class StaffChatCommand implements ICommandExecutor {
                             .userPreferenceService()
                             .setPreferenceFor(pl, NucleusKeysProvider.VIEW_STAFF_CHAT, true);
                 } else {
-                    StaffChatMessageChannel.getInstance()
-                            .sendMessageFrom(context.getCommandSourceRoot(),
-                                    context.getServiceCollection().textStyleService().addUrls(toSend.get()));
+                    frame.pushCause(context.getCommandSourceUnchecked());
+                    // mostly to allow plugins to know that's what we're doing.
+                    try (NoExceptionAutoClosable c = this.chatMessageFormatterService
+                            .setAudienceNucleusChannelTemporarily(context.getCommandSource(), StaffChatMessageChannel.getInstance())) {
+                        StaffChatMessageChannel.getInstance()
+                                .sendMessageFrom(context.getCommandSource(),
+                                        context.getServiceCollection().textStyleService().addUrls(toSend.get()));
+                    }
                 }
 
                 return context.successResult();