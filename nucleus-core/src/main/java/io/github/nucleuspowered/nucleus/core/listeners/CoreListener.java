NucleusPowered/Nucleus

Rework first join mechanics. Partially solves #1540

nucleus-core/src/main/java/io/github/nucleuspowered/nucleus/modules/core/listeners/CoreListener.java
@@ -5,6 +5,7 @@
 package io.github.nucleuspowered.nucleus.modules.core.listeners;
 
 import com.google.common.collect.Lists;
+import io.github.nucleuspowered.nucleus.Util;
 import io.github.nucleuspowered.nucleus.api.core.event.NucleusFirstJoinEvent;
 import io.github.nucleuspowered.nucleus.api.text.NucleusTextTemplate;
 import io.github.nucleuspowered.nucleus.modules.core.CoreKeys;
@@ -16,10 +17,14 @@ import io.github.nucleuspowered.nucleus.modules.core.services.UniqueUserService;
 import io.github.nucleuspowered.nucleus.scaffold.listener.ListenerBase;
 import io.github.nucleuspowered.nucleus.services.INucleusServiceCollection;
 import io.github.nucleuspowered.nucleus.services.impl.storage.dataobjects.modular.IUserDataObject;
+import io.github.nucleuspowered.nucleus.services.impl.storage.queryobjects.IUserQueryObject;
+import io.github.nucleuspowered.nucleus.services.impl.storage.services.UserService;
 import io.github.nucleuspowered.nucleus.services.interfaces.IMessageProviderService;
 import io.github.nucleuspowered.nucleus.services.interfaces.IReloadableService;
+import io.github.nucleuspowered.storage.services.IStorageService;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.command.CommandSource;
+import org.spongepowered.api.data.key.Keys;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.entity.living.player.User;
 import org.spongepowered.api.event.CauseStackManager;
@@ -40,7 +45,10 @@ import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import javax.annotation.Nullable;
@@ -52,6 +60,7 @@ public class CoreListener implements IReloadableService.Reloadable, ListenerBase
     @Nullable private NucleusTextTemplate getKickOnStopMessage = null;
     @Nullable private final URL url;
     private boolean warnOnWildcard = true;
+    private boolean checkSponge = false;
 
     @Inject
     public CoreListener(INucleusServiceCollection serviceCollection) {
@@ -74,7 +83,7 @@ public class CoreListener implements IReloadableService.Reloadable, ListenerBase
 
         // Create user data if required, and place into cache.
         // As this is already async, load on thread.
-        IUserDataObject dataObject = this.serviceCollection.storageManager().getUserService().getOrNewOnThread(userId);
+        final IUserDataObject dataObject = this.serviceCollection.storageManager().getUserService().getOrNewOnThread(userId);
 
         // Fire the event, which will be async too, perhaps unsurprisingly.
         // The main use for this will be migrations.
@@ -138,20 +147,32 @@ public class CoreListener implements IReloadableService.Reloadable, ListenerBase
     @Listener
     public void onPlayerJoinLast(final ClientConnectionEvent.Join event, @Getter("getTargetEntity") final Player player) {
         // created before
-        if (!this.serviceCollection.storageManager().getUserService().getOnThread(player.getUniqueId())
-                .map(x -> x.get(CoreKeys.FIRST_JOIN)).isPresent()) {
-            this.serviceCollection.getServiceUnchecked(UniqueUserService.class).resetUniqueUserCount();
+        final UUID uuid = player.getUniqueId();
+        final IStorageService.Keyed.KeyedData<UUID, IUserQueryObject, IUserDataObject> userService =
+                this.serviceCollection.storageManager().getUserService();
+        if (!userService
+                .getOnThread(uuid)
+                .flatMap(x -> x.get(CoreKeys.FIRST_JOIN_PROCESSED))
+                .orElse(false)) {
 
-            NucleusFirstJoinEvent firstJoinEvent = new OnFirstLoginEvent(
-                event.getCause(), player, event.getOriginalChannel(), event.getChannel().orElse(null), event.getOriginalMessage(),
-                    event.isMessageCancelled(), event.getFormatter());
+            if (!this.checkSponge || !Util.hasPlayedBeforeSponge(player)) {
+                this.serviceCollection.getServiceUnchecked(UniqueUserService.class).resetUniqueUserCount();
 
-            Sponge.getEventManager().post(firstJoinEvent);
-            event.setChannel(firstJoinEvent.getChannel().get());
-            event.setMessageCancelled(firstJoinEvent.isMessageCancelled());
-            this.serviceCollection.storageManager().getUserService()
-                    .getOrNew(player.getUniqueId())
-                    .thenAccept(x -> x.set(CoreKeys.FIRST_JOIN, x.get(CoreKeys.LAST_LOGIN).orElseGet(Instant::now)));
+                NucleusFirstJoinEvent firstJoinEvent = new OnFirstLoginEvent(
+                        event.getCause(), player, event.getOriginalChannel(), event.getChannel().orElse(null), event.getOriginalMessage(),
+                        event.isMessageCancelled(), event.getFormatter());
+
+                Sponge.getEventManager().post(firstJoinEvent);
+                event.setChannel(firstJoinEvent.getChannel().get());
+                event.setMessageCancelled(firstJoinEvent.isMessageCancelled());
+            }
+
+            userService.getOrNew(player.getUniqueId())
+                    .thenAccept(x -> {
+                        x.set(CoreKeys.FIRST_JOIN, x.get(CoreKeys.LAST_LOGIN).orElseGet(Instant::now));
+                        x.set(CoreKeys.FIRST_JOIN_PROCESSED, true);
+                        userService.save(uuid, x);
+                    });
         }
 
         // Warn about wildcard.
@@ -211,6 +232,7 @@ public class CoreListener implements IReloadableService.Reloadable, ListenerBase
         CoreConfig c = this.serviceCollection.moduleDataProvider().getModuleConfig(CoreConfig.class);
         this.getKickOnStopMessage = c.isKickOnStop() ? c.getKickOnStopMessage() : null;
         this.warnOnWildcard = c.isCheckForWildcard();
+        this.checkSponge = c.isCheckFirstDatePlayed();
     }
 
     @Listener

nucleus-core/src/main/java/io/github/nucleuspowered/nucleus/modules/core/services/PlayerMetadataService.java
@@ -6,15 +6,22 @@ package io.github.nucleuspowered.nucleus.modules.core.services;
 
 import com.flowpowered.math.vector.Vector3d;
 import com.google.inject.Inject;
+import io.github.nucleuspowered.nucleus.Util;
 import io.github.nucleuspowered.nucleus.api.core.NucleusPlayerMetadataService;
 import io.github.nucleuspowered.nucleus.modules.core.CoreKeys;
+import io.github.nucleuspowered.nucleus.modules.core.CorePermissions;
+import io.github.nucleuspowered.nucleus.modules.core.config.CoreConfig;
 import io.github.nucleuspowered.nucleus.scaffold.service.ServiceBase;
 import io.github.nucleuspowered.nucleus.scaffold.service.annotations.APIService;
 import io.github.nucleuspowered.nucleus.services.INucleusServiceCollection;
 import io.github.nucleuspowered.nucleus.services.impl.storage.dataobjects.modular.IUserDataObject;
+import io.github.nucleuspowered.nucleus.services.interfaces.IModuleDataProvider;
+import io.github.nucleuspowered.nucleus.services.interfaces.IPermissionService;
 import io.github.nucleuspowered.nucleus.services.interfaces.IStorageManager;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.entity.living.player.User;
+import org.spongepowered.api.service.user.UserStorageService;
 import org.spongepowered.api.util.Tuple;
 import org.spongepowered.api.util.annotation.NonnullByDefault;
 import org.spongepowered.api.world.Location;
@@ -31,15 +38,28 @@ import javax.annotation.Nullable;
 @NonnullByDefault
 public class PlayerMetadataService implements NucleusPlayerMetadataService, ServiceBase {
 
+    private final IPermissionService permissionService;
+    private final IModuleDataProvider moduleDataProvider;
     private final IStorageManager storageManager;
 
     @Inject
     public PlayerMetadataService(INucleusServiceCollection serviceCollection) {
         this.storageManager = serviceCollection.storageManager();
+        this.moduleDataProvider = serviceCollection.moduleDataProvider();
+        this.permissionService = serviceCollection.permissionService();
     }
 
     @Override public Optional<Result> getUserData(UUID uuid) {
-        return this.storageManager.getUserService().get(uuid).join().map(x -> new ResultImpl(uuid, x));
+        final Optional<User> user = Sponge.getServiceManager().provideUnchecked(UserStorageService.class).get(uuid);
+        final boolean isEligible;
+        if (user.isPresent()) {
+            final User u = user.get();
+            isEligible = !this.permissionService.hasPermission(u, CorePermissions.EXEMPT_FIRST_JOIN) &&
+                    (!this.moduleDataProvider.getModuleConfig(CoreConfig.class).isCheckFirstDatePlayed() || !Util.hasPlayedBeforeSponge(user.get()));
+        } else {
+            isEligible = true;
+        }
+        return this.storageManager.getUserService().get(uuid).join().map(x -> new ResultImpl(uuid, x, isEligible));
     }
 
     public static class ResultImpl implements Result {
@@ -50,14 +70,20 @@ public class PlayerMetadataService implements NucleusPlayerMetadataService, Serv
         @Nullable private final Instant login;
         @Nullable private final Instant logout;
         @Nullable private final String lastIP;
+        private final boolean firstJoinProcess;
 
-        private ResultImpl(UUID uuid, IUserDataObject udo) {
+        private ResultImpl(UUID uuid, IUserDataObject udo, boolean eligibleForFirstJoin) {
             // this.user = userService.getUser();
 
             this.uuid = uuid;
+            this.firstJoinProcess = eligibleForFirstJoin && !udo.get(CoreKeys.FIRST_JOIN_PROCESSED).orElse(false);
             this.login = udo.get(CoreKeys.LAST_LOGIN).orElse(null);
             this.logout = udo.get(CoreKeys.LAST_LOGOUT).orElse(null);
             this.lastIP = udo.get(CoreKeys.IP_ADDRESS).orElse(null);
+        }
+
+        @Override public boolean hasFirstJoinBeenProcessed() {
+            return this.firstJoinProcess;
         }
 
         @Override public Optional<Instant> getLastLogin() {

OpalSoPL/Oleus

nucleus-core/src/main/java/io/github/nucleuspowered/nucleus/core/listeners/CoreListener.java
@@ -4,7 +4,8 @@
  */
 package io.github.nucleuspowered.nucleus.core.listeners;
 
-import com.google.common.collect.Lists;
+import com.google.inject.Inject;
+import io.github.nucleuspowered.nucleus.Util;
 import io.github.nucleuspowered.nucleus.api.core.event.NucleusFirstJoinEvent;
 import io.github.nucleuspowered.nucleus.api.text.NucleusTextTemplate;
 import io.github.nucleuspowered.nucleus.core.CoreKeys;
@@ -16,16 +17,18 @@ import io.github.nucleuspowered.nucleus.core.services.UniqueUserService;
 import io.github.nucleuspowered.nucleus.scaffold.listener.ListenerBase;
 import io.github.nucleuspowered.nucleus.services.INucleusServiceCollection;
 import io.github.nucleuspowered.nucleus.services.impl.storage.dataobjects.modular.IUserDataObject;
+import io.github.nucleuspowered.nucleus.services.impl.storage.queryobjects.IUserQueryObject;
 import io.github.nucleuspowered.nucleus.services.interfaces.IMessageProviderService;
 import io.github.nucleuspowered.nucleus.services.interfaces.IReloadableService;
 import io.github.nucleuspowered.nucleus.util.AdventureUtils;
+import io.github.nucleuspowered.storage.services.IStorageService;
 import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.event.ClickEvent;
 import net.kyori.adventure.text.format.NamedTextColor;
+import org.checkerframework.checker.nullness.qual.Nullable;
 import org.spongepowered.api.Server;
 import org.spongepowered.api.Sponge;
-import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.entity.living.player.User;
 import org.spongepowered.api.entity.living.player.server.ServerPlayer;
 import org.spongepowered.api.event.CauseStackManager;
@@ -37,7 +40,6 @@ import org.spongepowered.api.event.lifecycle.StoppingEngineEvent;
 import org.spongepowered.api.event.network.ServerSideConnectionEvent;
 import org.spongepowered.api.profile.GameProfile;
 import org.spongepowered.api.scheduler.Task;
-import org.spongepowered.api.service.pagination.PaginationService;
 
 import java.net.InetAddress;
 import java.net.MalformedURLException;
@@ -45,11 +47,7 @@ import java.net.URL;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
-
-import org.checkerframework.checker.nullness.qual.Nullable;
-import com.google.inject.Inject;
 
 public class CoreListener implements IReloadableService.Reloadable, ListenerBase {
 
@@ -57,6 +55,7 @@ public class CoreListener implements IReloadableService.Reloadable, ListenerBase
     @Nullable private NucleusTextTemplate getKickOnStopMessage = null;
     @Nullable private final URL url;
     private boolean warnOnWildcard = true;
+    private boolean checkSponge = false;
 
     @Inject
     public CoreListener(final INucleusServiceCollection serviceCollection) {
@@ -143,23 +142,32 @@ public class CoreListener implements IReloadableService.Reloadable, ListenerBase
     @Listener
     public void onPlayerJoinLast(final ServerSideConnectionEvent.Join event, @Getter("getPlayer") final ServerPlayer player) {
         // created before
-        // TODO: Fix this
-        if (!this.serviceCollection.storageManager().getUserService().getOnThread(player.getUniqueId())
-                .map(x -> x.get(CoreKeys.FIRST_JOIN)).isPresent()) {
-            this.serviceCollection.getServiceUnchecked(UniqueUserService.class).resetUniqueUserCount();
+        final UUID uuid = player.getUniqueId();
+        final IStorageService.Keyed.KeyedData<UUID, IUserQueryObject, IUserDataObject> userService =
+                this.serviceCollection.storageManager().getUserService();
+        if (!userService
+                .getOnThread(uuid)
+                .flatMap(x -> x.get(CoreKeys.FIRST_JOIN_PROCESSED))
+                .orElse(false)) {
 
-            final NucleusFirstJoinEvent firstJoinEvent = new OnFirstLoginEvent(
-                event.getCause(), player, event.getOriginalAudience(),
-                    event.getAudience().orElseGet(Sponge::getSystemSubject),
-                    event.getOriginalMessage(),
-                    event.isMessageCancelled());
+            if (!this.checkSponge || !Util.hasPlayedBeforeSponge(player.getUser())) {
+                this.serviceCollection.getServiceUnchecked(UniqueUserService.class).resetUniqueUserCount();
 
-            Sponge.getEventManager().post(firstJoinEvent);
-            firstJoinEvent.getAudience().ifPresent(event::setAudience);
-            event.setMessageCancelled(firstJoinEvent.isMessageCancelled());
-            this.serviceCollection.storageManager().getUserService()
-                    .getOrNew(player.getUniqueId())
-                    .thenAccept(x -> x.set(CoreKeys.FIRST_JOIN, x.get(CoreKeys.LAST_LOGIN).orElseGet(Instant::now)));
+                final NucleusFirstJoinEvent firstJoinEvent = new OnFirstLoginEvent(
+                        event.getCause(), player, event.getOriginalAudience(), event.getAudience().orElse(null), event.getOriginalMessage(),
+                        event.isMessageCancelled());
+
+                Sponge.getEventManager().post(firstJoinEvent);
+                event.setAudience(firstJoinEvent.getAudience().get());
+                event.setMessageCancelled(firstJoinEvent.isMessageCancelled());
+            }
+
+            userService.getOrNew(player.getUniqueId())
+                    .thenAccept(x -> {
+                        x.set(CoreKeys.FIRST_JOIN, x.get(CoreKeys.LAST_LOGIN).orElseGet(Instant::now));
+                        x.set(CoreKeys.FIRST_JOIN_PROCESSED, true);
+                        userService.save(uuid, x);
+                    });
         }
 
         // Warn about wildcard.
@@ -211,6 +219,7 @@ public class CoreListener implements IReloadableService.Reloadable, ListenerBase
         final CoreConfig c = this.serviceCollection.configProvider().getModuleConfig(CoreConfig.class);
         this.getKickOnStopMessage = c.isKickOnStop() ? c.getKickOnStopMessage() : null;
         this.warnOnWildcard = c.isCheckForWildcard();
+        this.checkSponge = c.isCheckFirstDatePlayed();
     }
 
     @Listener

nucleus-core/src/main/java/io/github/nucleuspowered/nucleus/core/services/PlayerMetadataService.java
@@ -4,22 +4,24 @@
  */
 package io.github.nucleuspowered.nucleus.core.services;
 
+import io.github.nucleuspowered.nucleus.core.CorePermissions;
+import io.github.nucleuspowered.nucleus.services.interfaces.IConfigProvider;
 import org.spongepowered.api.entity.living.player.server.ServerPlayer;
 import org.spongepowered.api.world.ServerLocation;
 import org.spongepowered.math.vector.Vector3d;
 import com.google.inject.Inject;
+import io.github.nucleuspowered.nucleus.Util;
 import io.github.nucleuspowered.nucleus.api.core.NucleusPlayerMetadataService;
 import io.github.nucleuspowered.nucleus.core.CoreKeys;
 import io.github.nucleuspowered.nucleus.scaffold.service.ServiceBase;
 import io.github.nucleuspowered.nucleus.scaffold.service.annotations.APIService;
 import io.github.nucleuspowered.nucleus.services.INucleusServiceCollection;
 import io.github.nucleuspowered.nucleus.services.impl.storage.dataobjects.modular.IUserDataObject;
+import io.github.nucleuspowered.nucleus.services.interfaces.IPermissionService;
 import io.github.nucleuspowered.nucleus.services.interfaces.IStorageManager;
 import org.spongepowered.api.Sponge;
-import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.entity.living.player.User;
 import org.spongepowered.api.util.Tuple;
-import org.spongepowered.api.world.Location;
-import org.spongepowered.api.world.World;
 import org.spongepowered.api.world.storage.WorldProperties;
 
 import java.time.Instant;
@@ -31,15 +33,28 @@ import org.checkerframework.checker.nullness.qual.Nullable;
 @APIService(NucleusPlayerMetadataService.class)
 public class PlayerMetadataService implements NucleusPlayerMetadataService, ServiceBase {
 
+    private final IPermissionService permissionService;
+    private final IConfigProvider configProvider;
     private final IStorageManager storageManager;
 
     @Inject
     public PlayerMetadataService(final INucleusServiceCollection serviceCollection) {
         this.storageManager = serviceCollection.storageManager();
+        this.configProvider = serviceCollection.configProvider();
+        this.permissionService = serviceCollection.permissionService();
     }
 
     @Override public Optional<Result> getUserData(final UUID uuid) {
-        return this.storageManager.getUserService().get(uuid).join().map(x -> new ResultImpl(uuid, x));
+        final Optional<User> user = Sponge.getServer().getUserManager().get(uuid);
+        final boolean isEligible;
+        if (user.isPresent()) {
+            final User u = user.get();
+            isEligible = !this.permissionService.hasPermission(u, CorePermissions.EXEMPT_FIRST_JOIN) &&
+                    (!this.configProvider.getCoreConfig().isCheckFirstDatePlayed() || !Util.hasPlayedBeforeSponge(user.get()));
+        } else {
+            isEligible = true;
+        }
+        return this.storageManager.getUserService().get(uuid).join().map(x -> new ResultImpl(uuid, x, isEligible));
     }
 
     public static final class ResultImpl implements Result {
@@ -50,14 +65,20 @@ public class PlayerMetadataService implements NucleusPlayerMetadataService, Serv
         @Nullable private final Instant login;
         @Nullable private final Instant logout;
         @Nullable private final String lastIP;
+        private final boolean firstJoinProcess;
 
-        private ResultImpl(final UUID uuid, final IUserDataObject udo) {
+        private ResultImpl(final UUID uuid, final IUserDataObject udo, final boolean eligibleForFirstJoin) {
             // this.user = userService.getUser();
 
             this.uuid = uuid;
+            this.firstJoinProcess = eligibleForFirstJoin && !udo.get(CoreKeys.FIRST_JOIN_PROCESSED).orElse(false);
             this.login = udo.get(CoreKeys.LAST_LOGIN).orElse(null);
             this.logout = udo.get(CoreKeys.LAST_LOGOUT).orElse(null);
             this.lastIP = udo.get(CoreKeys.IP_ADDRESS).orElse(null);
+        }
+
+        @Override public boolean hasFirstJoinBeenProcessed() {
+            return this.firstJoinProcess;
         }
 
         @Override public Optional<Instant> getLastLogin() {